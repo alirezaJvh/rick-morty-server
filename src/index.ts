70bec924d3b13819333aeea7818a84a9a4947912
alirezaJavaheri <alireza.jvh98@gmail.com>
2023-06-01 12:26:46 +0330

    build: init graphql-yogo playground

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-function test(a: string) {
-  console.log('helo');
-  console.log(a);
-}
-
-test('salam');
--- src/schema.ts
+++ src/schema.ts
@@ -1,18 +1,35 @@
-import { makeExecutableSchema } from '@graphql-tools/schema'
- 
+import { makeExecutableSchema } from '@graphql-tools/schema';
+
+type User = {
+  username: string;
+};
+
+const user: User = { username: 'alice' };
+
 const typeDefinitions = /* GraphQL */ `
+  type User {
+    username: String!
+  }
+
   type Query {
     hello: String!
   }
-`
- 
+
+  type Mutation {
+    createUser(username: String!): User!
+  }
+`;
+
 const resolvers = {
   Query: {
-    hello: () => 'Hello World!'
-  }
-}
- 
+    hello: () => 'Hello World!',
+  },
+  Mutation: {
+    createUser: () => user,
+  },
+};
+
 export const schema = makeExecutableSchema({
   resolvers: [resolvers],
-  typeDefs: [typeDefinitions]
-})
+  typeDefs: [typeDefinitions],
+});
