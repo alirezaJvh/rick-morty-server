1e89b6009ac1b531577ddc628d090ac36bdaa80a
alirezaJavaheri <alireza.jvh98@gmail.com>
2023-06-08 08:46:10 +0330

    feat: add currentUser in context

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
 import 'dotenv/config';
-import { createYoga } from 'graphql-yoga';
+import { YogaInitialContext, createYoga } from 'graphql-yoga';
 import { createServer } from 'http';
 import { schema } from './schema';
 import { connectMongo } from './database';
+import jwt from 'jsonwebtoken';
 
 const { PORT } = process.env;
 
 connectMongo();
 
-const yoga = createYoga({ schema });
+const yoga = createYoga({
+  schema,
+  context(initicalContext: YogaInitialContext) {
+    const { request } = initicalContext;
+    const auth = request ? request.headers.get('authorization') : null;
+    if (auth && auth.startsWith('Bearer')) {
+      const token = auth.split(' ')[1];
+      const decodedToken = jwt.verify(token, 'kYK2FZr$B8ytGy^G5T@+');
+      return { currentUser: decodedToken };
+    }
+  },
+});
 const server = createServer(yoga);
 
 server.listen(PORT, () => {
