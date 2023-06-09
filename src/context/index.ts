c5dc537b43a522b5c2c64707ffd614decee556db
alirezaJavaheri <alireza.jvh98@gmail.com>
2023-06-09 10:57:20 +0330

    refactor: context and index file

---
+++ src/context/index.ts
@@ -0,0 +1,41 @@
+import { GraphQLParams, YogaInitialContext } from 'graphql-yoga';
+import jwt from 'jsonwebtoken';
+import { RedisClientType } from 'redis';
+
+interface GraphQLContext extends YogaInitialContext {
+  redisClient: RedisClientType;
+  cache: any;
+  currentUser: any;
+}
+
+const createContext = (redisClient: RedisClientType) => {
+  return context(redisClient);
+};
+
+const checkUser = (request: Request) => {
+  const auth = request ? request.headers.get('authorization') : null;
+  if (auth && auth.startsWith('Bearer')) {
+    const token = auth.split(' ')[1];
+    const decodedToken = jwt.verify(token, 'kYK2FZr$B8ytGy^G5T@+');
+    return decodedToken;
+  }
+};
+
+const checkCache = async (params: GraphQLParams, redisClient: RedisClientType) => {
+  if (params.operationName === 'Characters' && redisClient) {
+    const redisKey = `${params.operationName}:${params.variables?.page}`;
+    const jsonCache = await redisClient.get(redisKey);
+    return jsonCache !== null ? JSON.parse(jsonCache) : undefined;
+  }
+};
+
+const context =
+  (redisClient: RedisClientType) =>
+  async (initicalContext: YogaInitialContext): Promise<GraphQLContext> => {
+    const { request, params } = initicalContext;
+    const cache: any = await checkCache(params, redisClient);
+    const currentUser: any = checkUser(request);
+    return { ...initicalContext, currentUser, cache, redisClient };
+  };
+
+export { createContext };
--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,18 @@ import 'dotenv/config';
 import { YogaInitialContext, createYoga } from 'graphql-yoga';
 import { createServer } from 'http';
 import { schema } from './schema';
-import { connectMongo } from './database';
-import jwt from 'jsonwebtoken';
+import { createContext } from './context';
+import { connectDatabase } from './database';
 
 const { PORT } = process.env;
 
-connectMongo();
+(async () => {
+  const redisClient = await connectDatabase();
+  const context = createContext(redisClient);
+  const yoga = createYoga({ schema, context });
+  const server = createServer(yoga);
 
-const yoga = createYoga({
-  schema,
-  context(initicalContext: YogaInitialContext) {
-    const { request } = initicalContext;
-    const auth = request ? request.headers.get('authorization') : null;
-    if (auth && auth.startsWith('Bearer')) {
-      const token = auth.split(' ')[1];
-      const decodedToken = jwt.verify(token, 'kYK2FZr$B8ytGy^G5T@+');
-      return { currentUser: decodedToken };
-    }
-  },
-});
-const server = createServer(yoga);
-
-server.listen(PORT, () => {
-  console.info(`🚀 Server is running on http://localhost:${PORT}/graphql`);
-});
+  server.listen(PORT, () => {
+    console.info(`🚀 Server is running on http://localhost:${PORT}/graphql`);
+  });
+})();
